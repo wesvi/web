138474f1c680e68be4d303fc5a28f5d0aeccc0fe
wesvi <36145051+wesvi@users.noreply.github.com>
2023-01-30 14:12:20 +0800

    garden

--- garden/gatsby-config.js
+++ garden/gatsby-config.js
@@ -7,15 +7,15 @@ require("dotenv").config({
 module.exports = {
   pathPrefix: `/gatsby-digital-garden`,
   siteMetadata: {
-    title: `Digital Garden`,
-    description: `Example of using Roam Research as a data source for a Gatsby site`,
+    title: `Wesvi' Digital Garden`,
+    description: `Wesvi' Digital Garden of using Roam Research as a data source for a Gatsby site`,
   },
   plugins: [
     {
       resolve: `gatsby-theme-garden`,
       options: {
-        rootNote: "/About-these-notes",
-        contentPath: `${__dirname}/notes`,
+        rootNote: "/README",
+        contentPath: `${__dirname}/note`,
         roamUrl: process.env.ROAM_URL,
         roamEmail: process.env.ROAM_EMAIL,
         roamPassword: process.env.ROAM_PASSWORD,
