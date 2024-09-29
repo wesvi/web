83021aee20a5297afe02846264fb6a9692f30f2d
wesvi <sctengxin@gmail.com>
2024-09-29 17:38:43 +0800

    20240929

---
+++ z/content/communities/customer-experience-community.md
@@ -0,0 +1,17 @@
+---
+url: /communities/customer-experience-community/
+uid: customer-experience-community
+date: 2015-07-08 10:30:37 -0400
+title: 'Customer Experience Community of Practice'
+summary: 'The government Customer Experience Community of Practice (CX-COP) is an interagency group of customer experience practitioners, with over 700 members across more than 140 federal, state and local U.S. government offices and agencies.'
+aliases:
+  - /customer-experience-community/
+---
+
+The government Customer Experience Community of Practice (CX-COP) is an interagency group of customer experience practitioners, with over 700 members across more than 140 federal, state and local U.S. government offices and agencies.
+
+### Join
+
+Join the CX-COP listserv and community to stay on the cutting edge of government customer experience, including news, training, best practices, and technology.
+
+Fill out the [sign-up form](https://docs.google.com/a/gsa.gov/forms/d/1hzJbZChUg2TRLi_MiC4nAbB-HKUOerBF2kL0qO38fPo/viewform), or [e-mail us with your request](mailto:annabel.berman@gsa.gov?subject=Subscribe%20to%20CX-COP). You must have a government e-mail address to join.
---
+++ z/themes/digital.gov/static/js/external.js
@@ -0,0 +1,59 @@
+jQuery(document).ready(function($) {
+
+
+// Transforms the Edit link on posts/pages/events to point to the GitHub file
+function transform_edit_file_link(){
+	// If there is an edit link on the page
+	if ( $('.entry .edit_file').length ) {
+		var data = $('.edit_file'); // get the edit link data
+		var filepath = $(data).attr('data-filepath'); // the filename in GitHub
+		var github_file_path = 'https://github.com/GSA/digitalgov.gov/edit/master/content/'+filepath;
+		$(data).attr('href', github_file_path);
+	}
+}
+transform_edit_file_link();
+});
+
+
+
+function externalLinks() {
+	if (!document.getElementsByTagName) return;
+	var anchors = document.getElementsByTagName("a");
+	for (var i=0; i<anchors.length; i++) {
+		var anchor = anchors[i];
+		if (anchor.getAttribute("href") &&
+			anchor.getAttribute("rel") == "external")
+			anchor.target = "_blank";
+	}
+}
+
+window.onload = externalLinks;
+
+jQuery(window).load(function() {
+	jQuery("#topofpage").hide().removeAttr("href");
+	if (jQuery(window).scrollTop() != "0")
+		jQuery("#backtotop").fadeIn("fast")
+	var scrollDiv = jQuery("#backtotop");
+	jQuery(window).scroll(function(){
+		if (jQuery(window).scrollTop() == "0")
+			jQuery(scrollDiv).fadeOut("fast")
+		else
+			jQuery(scrollDiv).fadeIn("fast")
+	});
+	jQuery("#backtotop").click(function() {
+		jQuery("html, body").animate({
+			scrollTop: 0
+		}, "fast")
+	})
+});
+
+jQuery(function( $ ){
+	jQuery(window).scroll(function() {
+		var yPos = ( jQuery(window).scrollTop() );
+		if(yPos > 200) { // show fixed nav bar after screen has scrolled down 200px from the top
+			jQuery("#fixednav").fadeIn('fast');
+		} else {
+			jQuery("#fixednav").fadeOut('fast');
+		}
+	});
+});
